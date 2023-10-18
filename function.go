236ab31810d4e1a7f0c89cdfbf80c2bb1f6ecb41
Mystlicious <Daffaaudyapramana03@gmail.com>
2023-10-18 09:09:46 +0700

    ay

---
+++ function.go
@@ -0,0 +1,18 @@
+package gcp
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/petapedia/peda"
+)
+
+func init() {
+	functions.HTTP("GeoJson", petaPedia)
+}
+
+func petaPedia(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "https://daffaaudyapramana.github.io")
+	fmt.Fprintf(w, peda.GCFHandler("MONGO_URI", "geojson", "cimahi"))
+}
